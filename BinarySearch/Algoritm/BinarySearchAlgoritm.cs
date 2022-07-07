5fd8a8c6df7d41d70d4f7c4e60e6b2fb1ec9dec2
Gosha94 <gerizch@rambler.ru>
2022-07-08 00:15:42 +0300

    add binary search algoritm

---
+++ BinarySearch/Algoritm/BinarySearchAlgoritm.cs
@@ -0,0 +1,35 @@
+﻿namespace BinarySearch.Algoritm
+{
+    public class BinarySearchAlgoritm
+    {
+        /// <summary>
+        /// Метод двоичного поиска
+        /// </summary>
+        public static int BinarySearch(int[] array, int searchValue)
+        {
+            var lowBorderIndex = 0;
+            var highBorderIndex = array.Length - 1;
+
+            while (lowBorderIndex <= highBorderIndex)
+            {
+                var midIndex = (lowBorderIndex + highBorderIndex) / 2;
+                var currentValue = array[midIndex];
+
+                if (currentValue == searchValue)
+                {
+                    return midIndex;
+                }
+                else if (currentValue > searchValue)
+                {
+                    highBorderIndex = midIndex - 1;
+                }
+                else
+                {
+                    lowBorderIndex = midIndex + 1;
+                }
+            }
+            // Если элемент в массиве не найден
+            return -1;
+        }
+    }
+}
--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BinarySearch.Algoritm;
+
+//var inputArr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//var result1 = BinarySearchAlgoritm.BinarySearch(inputArr, 3);
+
+
+
+Console.ReadLine();
